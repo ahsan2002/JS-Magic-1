1ea63692e1a830329082a301b17904c1ec54ef2e
Ahsan <omerjee.ahsan@gmail.com>
2022-07-03 22:57:54 +0500

    ready

---
+++ app.js
@@ -0,0 +1,17 @@
+var c=document.getElementsByTagName("body")[0];
+function chcolor() {
+    var a=prompt("Enter your color");
+    c.style.backgroundColor=a;
+    
+
+}
+
+
+// var body = document.getElementsByTagName('body')[0];
+// function changeColor()
+// {
+// var color = document.getElementById('color').value;
+    
+// body.style.backgroundColor=color;    
+    
+// }
